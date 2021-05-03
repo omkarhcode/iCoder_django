5193a7dce35b1263afa383daf1de9edd27882278
omkarhcode <omkarh.code@gmail.com>
2021-05-04 01:15:39 +0530

    added contact form and alert message

--- home/views.py
+++ home/views.py
@@ -1,13 +1,24 @@
 from django.shortcuts import render, HttpResponse
+from django.contrib import messages
+from home.models import Contact
 
 # Create your views here.
 def home(request):
 	return render(request, 'home/home.html')
-	# return HttpResponse("This is home")
 
 def about(request):
 	return render(request, 'home/about.html')
 
 def contact(request):
+	if request.method == "POST":
+		name = request.POST.get('name','')
+		email = request.POST.get('email','')
+		phone =  request.POST.get('phone','')
+		content = request.POST.get('content','')
+		if len(name) < 2 or len(email)<3 or len(phone)<10 or len(content)<4:
+			messages.error(request, "Please fill the form correctly!")
+		else:
+			contact = Contact(name=name, email=email, phone=phone, content=content)
+			contact.save()
+			messages.success(request, "Your message has been sent!")
 	return render(request, 'home/contact.html')
-
