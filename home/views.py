64131d22ac9d30033a85c89904de6c63a0da2246
omkarhcode <omkarh.code@gmail.com>
2021-05-10 02:35:18 +0530

    added login logout functionality

--- home/views.py
+++ home/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, HttpResponse, redirect
 from django.contrib import messages
 from django.contrib.auth.models import User
-
+from django.contrib.auth import authenticate, login, logout
 
 # custom models
 from home.models import Contact
@@ -61,15 +61,26 @@ def handleSignup(request):
 		pass2 = request.POST['pass2']
 
 		# Check for errorneous inputs
-		# 
+		# Username should not be > 10 characters
+		if len(username) > 10:
+			messages.error(request, "Username must be under 10 character")
+			return redirect('home')
+
+		if not username.isalnum():
+			messages.error(request, "Username should only contain letters and numbers")
+			return redirect('home')
+
+		if pass1 != pass2:
+			messages.error(request, "Passwords do not match")
+			return redirect('home')
 
 		# Create the user
 		myuser = User.objects.create_user(username, email, pass1)
 		myuser.first_name = fname
 		myuser.last_name = lname
 		myuser.save()
+		
 		messages.success(request, "Your iCoder account has been successfully created.")
-
 		return redirect('home')
 
 	else:
@@ -78,6 +89,35 @@ def handleSignup(request):
 
 
 
+def handleLogin(request):
+
+	if request.method == 'POST':
+		# Get the post parameters
+		loginusername = request.POST['loginusername']
+		loginpassword = request.POST['loginpassword']
+
+		# User authentication
+		user = authenticate(username=loginusername, password=loginpassword)
+
+		if user is not None:
+			login(request, user)
+			messages.success(request, "successfully Logged in. ")
+			return redirect('home')
+		else:
+			messages.error(request, "Invalid Credentials, please try again ")
+			return redirect('home')
+
+	else:
+		return HttpResponse('404 - Not Found')
+
+
+
+	return HttpResponse('HandleLogin')
+
+def handleLogout(request):
+	logout(request)
+	messages.success(request, "successfully logged out. ")
+	return redirect('home')
 
 
 
