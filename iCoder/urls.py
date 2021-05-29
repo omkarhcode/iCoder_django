02e9cad5120fa970fc66b7544d0fdf15b59124ec
omkarhcode <omkarh.code@gmail.com>
2021-05-29 13:43:42 +0530

    added missing comma

--- iCoder/urls.py
+++ iCoder/urls.py
@@ -23,5 +23,5 @@ admin.site.index_title = "Welcome to iCoder Admin Panel"
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('home.urls')),
-    path('blog/', include('blog.urls'))
+    path('blog/', include('blog.urls')),
 ]
