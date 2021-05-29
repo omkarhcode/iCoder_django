34084f877dadbdb068ae53e05922f77763ca15c7
omkarhcode <omkarh.code@gmail.com>
2021-05-29 13:52:54 +0530

    added missing comma.

--- iCoder/settings.py
+++ iCoder/settings.py
@@ -44,7 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'django.contrib.humanize',
     'home.apps.HomeConfig',
-    'blog.apps.BlogConfig'
+    'blog.apps.BlogConfig',
 ]
 
 MIDDLEWARE = [
